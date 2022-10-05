015e89eaf392e7b4df5c2f65ba21a2086ee105e9
GiCheols <ngc4665@naver.com>
2022-10-06 03:48:41 +0900

    Task1

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-    private static TV tv;
-    private static Scanner s = new Scanner(System.in);
-    public static void main(String[] args) {
+    private TV init(){
+        Scanner s = new Scanner(System.in);
         System.out.println("TV 이름을 정해주세요>> ");
-        tv = new TV(s.next());
+        return new TV(s.next());
+    }
+    private void run(){
+        TV tv = init();
         tv.channelUp();
         tv.powerChange();
         tv.channelDown();
         tv.volumeUp();
         tv.volumeDown();
-
         tv.powerChange();
+    }
+    public static void main(String[] args) {
+        Main tv = new Main();
+        tv.run();
     }
 }
